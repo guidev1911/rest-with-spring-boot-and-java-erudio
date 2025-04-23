c1f81403d7bba72fb2babdb9a1f76bc68fcb5fa8
guidev1911 <guilhermereal1911@gmail.com>
2025-04-22 23:35:26 -0300

    endpoint findById

--- src/main/java/br/com/erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -1,5 +1,6 @@
 package br.com.erudio.services;
 
+import br.com.erudio.model.Person;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -11,4 +12,15 @@ public class PersonServices {
 
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+    public Person findById(String id){
+        logger.info("Finding one person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Guilherme");
+        person.setLastName("Brito");
+        person.setAddress("São Cristovão");
+        person.setGender("Male");
+        return person;
+    }
 }
