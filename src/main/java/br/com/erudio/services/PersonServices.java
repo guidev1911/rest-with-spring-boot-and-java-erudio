12d591208df609e75e5d28e8115ace9e22e3cfd0
guidev1911 <guilhermereal1911@gmail.com>
2025-04-24 23:31:35 -0300

    create person

--- src/main/java/br/com/erudio/services/PersonServices.java
+++ src/main/java/br/com/erudio/services/PersonServices.java
@@ -15,6 +15,8 @@ public class PersonServices {
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
     public List<Person> findAll(){
+        logger.info("Finding one person!");
+
         List<Person> persons = new ArrayList<>();
         for (int i = 0; i < 8; i++) {
             persons.add(mockPerson(i));
@@ -31,6 +33,11 @@ public class PersonServices {
         person.setLastName("Brito");
         person.setAddress("São Cristovão");
         person.setGender("Male");
+        return person;
+    }
+    public Person create(Person person){
+        logger.info("Creating one person!");
+
         return person;
     }
     private Person mockPerson(int i){
