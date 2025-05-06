5ba58149290707d76eeb73b962fed03d7bf2b956
guidev1911 <guilhermereal1911@gmail.com>
2025-05-05 23:46:00 -0300

    simular data

--- src/main/java/br/com/erudio/controllers/PersonController.java
+++ src/main/java/br/com/erudio/controllers/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -25,7 +26,10 @@ public class PersonController {
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public PersonDTO findById(@PathVariable("id") Long id) {
-        return service.findById(id);
+
+        var person = service.findById(id);
+        person.setBirthday(new Date());
+        return person;
     }
 
     @PostMapping(
