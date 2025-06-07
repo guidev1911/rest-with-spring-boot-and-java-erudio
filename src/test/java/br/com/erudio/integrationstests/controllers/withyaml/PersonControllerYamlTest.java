757cb10b789588f4dac447cc2a8007d97bd83d47
guidev1911 <guilhermereal1911@gmail.com>
2025-06-06 23:50:55 -0300

    corrigindo findAll yml

--- src/test/java/br/com/erudio/integrationstests/controllers/withyaml/PersonControllerYamlTest.java
+++ src/test/java/br/com/erudio/integrationstests/controllers/withyaml/PersonControllerYamlTest.java
@@ -3,6 +3,7 @@ package br.com.erudio.integrationstests.controllers.withyaml;
 import br.com.erudio.config.TestConfigs;
 import br.com.erudio.integrationstests.controllers.withyaml.mapper.YAMLMapper;
 import br.com.erudio.integrationstests.dto.PersonDTO;
+import br.com.erudio.integrationstests.dto.wrappers.xml.PagedModelPerson;
 import br.com.erudio.integrationstests.testcontainers.AbstractIntegrationTest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.restassured.builder.RequestSpecBuilder;
@@ -197,14 +198,14 @@ class PersonControllerYamlTest extends AbstractIntegrationTest {
                 .then()
                 .statusCode(204);
     }
-
-
+    
     @Test
     @Order(6)
     void findAllTest() throws JsonProcessingException {
 
         var response = given(specification)
                 .accept(MediaType.APPLICATION_YAML_VALUE)
+                .queryParams("page", 3, "size", 12, "direction", "asc")
                 .when()
                 .get()
                 .then()
@@ -212,31 +213,31 @@ class PersonControllerYamlTest extends AbstractIntegrationTest {
                 .contentType(MediaType.APPLICATION_YAML_VALUE)
                 .extract()
                 .body()
-                .as(PersonDTO[].class, objectMapper);
+                .as(PagedModelPerson.class, objectMapper);
 
-        List<PersonDTO> people = Arrays.asList(response);
+        List<PersonDTO> people = response.getContent();
 
         PersonDTO personOne = people.get(0);
 
         assertNotNull(personOne.getId());
         assertTrue(personOne.getId() > 0);
 
-        assertEquals("Ayrton", personOne.getFirstName());
-        assertEquals("Senna", personOne.getLastName());
-        assertEquals("São Paulo - Brasil", personOne.getAddress());
+        assertEquals("Allin", personOne.getFirstName());
+        assertEquals("Emmot", personOne.getLastName());
+        assertEquals("7913 Lindbergh Way", personOne.getAddress());
         assertEquals("Male", personOne.getGender());
-        assertTrue(personOne.getEnabled());
+        assertFalse(personOne.getEnabled());
 
         PersonDTO personFour = people.get(4);
 
         assertNotNull(personFour.getId());
         assertTrue(personFour.getId() > 0);
 
-        assertEquals("Muhamamd", personFour.getFirstName());
-        assertEquals("Ali", personFour.getLastName());
-        assertEquals("Kentucky - US", personFour.getAddress());
+        assertEquals("Alonso", personFour.getFirstName());
+        assertEquals("Luchelli", personFour.getLastName());
+        assertEquals("9 Doe Crossing Avenue", personFour.getAddress());
         assertEquals("Male", personFour.getGender());
-        assertTrue(personFour.getEnabled());
+        assertFalse(personFour.getEnabled());
     }
 
     private void mockPerson() {
