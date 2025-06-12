b90f3bbc358f1f873349cf9820344e99da7c8711
guidev1911 <guilhermereal1911@gmail.com>
2025-06-12 00:37:29 -0300

    testes findByName yml/xml

--- src/test/java/br/com/erudio/integrationstests/controllers/withyaml/mapper/YAMLMapper.java
+++ src/test/java/br/com/erudio/integrationstests/controllers/withyaml/mapper/YAMLMapper.java
@@ -1,5 +1,6 @@
 package br.com.erudio.integrationstests.controllers.withyaml.mapper;
 
+import br.com.erudio.integrationstests.dto.wrappers.xml.PagedModelPerson;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.type.TypeFactory;
