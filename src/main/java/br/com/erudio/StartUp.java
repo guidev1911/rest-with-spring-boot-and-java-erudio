d07b0e0c91dd342ff9a605094303cb9110d676d2
guidev1911 <guilhermereal1911@gmail.com>
2025-08-02 23:14:21 -0300

    envio de emails/com anexo/ builder fluent interface pattern

--- src/main/java/br/com/erudio/StartUp.java
+++ src/main/java/br/com/erudio/StartUp.java
@@ -1,5 +1,6 @@
 package br.com.erudio;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StartUp {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(StartUp.class, args);
 	}
-
 }
